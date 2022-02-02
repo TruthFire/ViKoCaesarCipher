08e2e66b7eb714a25eca9beb0d298117e833bec2
TruthFire <truth.fire@yandex.ru>
2022-02-02 21:20:19 +0200

    ASCII Letters only decrypt

--- CaesarCipher/Form1.cs
+++ CaesarCipher/Form1.cs
@@ -17,18 +17,15 @@ namespace CaesarCipher
             'A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k',
             'L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v',
             'W','w','X','x','Y','y','Z','z'
-        };
+        }; //Array of big and small letters
 
         
         public Form1()
         {
             InitializeComponent();
-
-            string input = "The quick brown fox jumps over the lazy dog";
-            string output = "";
-            int key = 3;
-            
         }
+
+        // Array
 
         protected string EncryptArray(string input, int key)
         {
@@ -65,22 +62,23 @@ namespace CaesarCipher
             foreach (char c in input)
             {
                 letterIndex = Array.IndexOf(Letters, c); //Get index of required letter in Letters array
-                if (letterIndex == -1)
+                if (letterIndex == -1) //If not letter
                 {
                     output += c;
                     continue;
                 }
-                else if (letterIndex - (key * 2) < 0)
+                else if (letterIndex - (key * 2) < 0) //If letter is in range
                 {
                     output += Letters[Letters.Length + (letterIndex-key*2)].ToString();
                     continue;
                 }
-                else if (letterIndex - key * 2 > 0) //
+                else if (letterIndex - key * 2 > 0) //If letter is out of range
                 {
                     EncryptedLetterIndex = letterIndex -  key * 2;
                     output += Letters[EncryptedLetterIndex].ToString();
+                    continue;
                 }
-                else
+                else 
                 {
                     output += Letters[0];
                 }
@@ -90,29 +88,38 @@ namespace CaesarCipher
 
         }
 
-        protected string EncryptASCII(string input, int key)
+        //ASCII
+
+        protected static bool IsASCIILetter(char c)
+        {
+            return ((int)c >= 65 && (int)c <= 90 || (int)c >= 97 && (int)c <= 122); 
+            //a - 65, z = 90, A = 97, Z = 122;
+        }
+
+        protected static string EncryptASCII_Letters(string input, int key)
         {
             string output = "";
 
             foreach(char c in input)
             {
-                if ((int)c >= 65 && (int)c <= 90 || (int)c >= 97 && (int)c <= 122)
+                if (IsASCIILetter(c)) 
                 {
-                    if((int)c+key > 90 && (int)c+ key < 97)
+                    if((int)c+key > 90 && (int)c+ key < 97) //If big letter out of range
                     {
-                        output += Convert.ToChar(64 + ((int)c + key - 90)); //NOT FIXED :(
+                        output += Convert.ToChar(64 + ((int)c + key - 90));
                         continue;
                     }
-                    else if ((int)c + key > 122)
+                    else if ((int)c + key > 122) //If small letter out of range
                     {
                         output += Convert.ToChar(96 + ((int)c + key) - 122);
+                        continue;
                     }
-                    else
+                    else //In range
                     {
                         output += Convert.ToChar(((int)c + key));
                     }
                 }
-                else
+                else //not a letter
                 {
                     output += c;
                 }
@@ -122,16 +129,55 @@ namespace CaesarCipher
             return output;
         }
 
+        protected string DecryptASCII_Letters(string input, int key)
+        {
+            string output = "";
+
+            foreach(char c in input)
+            {
+                if(IsASCIILetter(c))
+                {
+                    if ((int)c - key < 65) //If big letter out of range
+                    {
+                        output += Convert.ToChar((int)c-key+26);
+                        continue;
+                    }
+                    else if ((int)c - key < 97 && (int)c - key > 90) //If small letter out of range
+                    {
+                        output += Convert.ToChar((int)c - key + 26);
+                        continue;
+                    }
+                    else
+                    {
+                        output += Convert.ToChar((int)c - 3);
+                    }
+                    
+
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+
+        //EVENTS 
+
         private void button1_Click(object sender, EventArgs e)
         {
-            this.textBox2.Text = EncryptASCII(this.textBox1.Text, (int)this.numericUpDown1.Value);
+            this.textBox2.Text = EncryptASCII_Letters(this.textBox1.Text, (int)this.numericUpDown1.Value);
             //this.textBox2.Text += EncryptArray(this.textBox1.Text, (int)this.numericUpDown1.Value);
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-          this.textBox1.Text = DecryptArray(this.textBox2.Text, (int)this.numericUpDown1.Value);
+            //this.textBox1.Text = DecryptArray(this.textBox2.Text, (int)this.numericUpDown1.Value);
+            //EncryptArray(this.textBox2.Text, 26-(int)this.numericUpDown1.Value - works too
+            this.textBox1.Text = DecryptASCII_Letters(this.textBox2.Text, (int)this.numericUpDown1.Value);
         }
     }
 }
