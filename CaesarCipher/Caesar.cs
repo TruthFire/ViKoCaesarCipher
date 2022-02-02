26cc168550d1581a48bac5b9c5bce723197d7b9a
TruthFire <truth.fire@yandex.ru>
2022-02-03 00:04:25 +0200

    Final, i guess

---
+++ CaesarCipher/Caesar.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    internal class Caesar
+    {
+        public Caesar()
+        {
+
+        }
+
+        char[] Letters =
+       {
+            'A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k',
+            'L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v',
+            'W','w','X','x','Y','y','Z','z'
+        }; //Array of big and small letters
+
+        // Array
+
+        public string EncryptArray(string input, int key)
+        {
+            string output = "";
+            int letterIndex, EncryptedLetterIndex;
+            foreach (char c in input)
+            {
+                letterIndex = Array.IndexOf(Letters, c); //Get index of required letter in Letters array
+                if (letterIndex == -1)
+                {
+                    output += c;
+                    continue;
+                }
+                else if (letterIndex <= Letters.Length - key * 2)
+                {
+                    output += Letters[letterIndex + key * 2].ToString();
+                    continue;
+                }
+                else if (letterIndex >= Letters.Length - key * 2) //
+                {
+                    EncryptedLetterIndex = letterIndex - (Letters.Length - key * 2);
+                    output += Letters[EncryptedLetterIndex].ToString();
+                }
+            }
+
+            return output;
+        }
+
+        public string DecryptArray(string input, int key)
+        {
+            string output = "";
+
+            int letterIndex, EncryptedLetterIndex;
+            foreach (char c in input)
+            {
+                letterIndex = Array.IndexOf(Letters, c); //Get index of required letter in Letters array
+                if (letterIndex == -1) //If not letter
+                {
+                    output += c;
+                    continue;
+                }
+                else if (letterIndex - (key * 2) < 0) //If letter is in range
+                {
+                    output += Letters[Letters.Length + (letterIndex - key * 2)].ToString();
+                    continue;
+                }
+                else if (letterIndex - key * 2 > 0) //If letter is out of range
+                {
+                    EncryptedLetterIndex = letterIndex - key * 2;
+                    output += Letters[EncryptedLetterIndex].ToString();
+                    continue;
+                }
+                else
+                {
+                    output += Letters[0];
+                }
+            }
+
+            return output;
+
+        }
+
+        //ASCII (Letters only)
+
+        protected static bool IsASCIILetter(char c)
+        {
+            return ((int)c >= 65 && (int)c <= 90 || (int)c >= 97 && (int)c <= 122);
+            //a - 65, z = 90, A = 97, Z = 122;
+        }
+
+        public string EncryptASCII_Letters(string input, int key)
+        {
+            string output = "";
+
+            foreach (char c in input)
+            {
+                if (IsASCIILetter(c))
+                {
+                    if ((int)c + key > 90 && (int)c + key < 97) //If big letter out of range
+                    {
+                        output += Convert.ToChar(64 + ((int)c + key - 90));
+                        continue;
+                    }
+                    else if ((int)c + key > 122) //If small letter out of range
+                    {
+                        output += Convert.ToChar(96 + ((int)c + key) - 122);
+                        continue;
+                    }
+                    else //In range
+                    {
+                        output += Convert.ToChar(((int)c + key));
+                    }
+                }
+                else //not a letter
+                {
+                    output += c;
+                }
+            }
+
+
+            return output;
+        }
+
+        public string DecryptASCII_Letters(string input, int key)
+        {
+            string output = "";
+
+            foreach (char c in input)
+            {
+                if (IsASCIILetter(c))
+                {
+                    if ((int)c - key < 65) //If big letter out of range
+                    {
+                        output += Convert.ToChar((int)c - key + 26);
+                        continue;
+                    }
+                    else if ((int)c - key < 97 && (int)c - key > 90) //If small letter out of range
+                    {
+                        output += Convert.ToChar((int)c - key + 26);
+                        continue;
+                    }
+                    else
+                    {
+                        output += Convert.ToChar((int)c - 3);
+                    }
+
+
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+        // ASCII (Letters and symbols)
+
+
+        protected static bool IsASCIIChar(char c)
+        {
+            return ((int)c >= 32 && (int)c <= 126);
+            //space - 32, ~ = 126
+        }
+
+        public string EncryptASCII(string input, int key)
+        {
+            string output = "";
+
+            foreach (char c in input)
+            {
+                if (IsASCIIChar(c))
+                {
+                    if ((int)c + key > 126)
+                    {
+                        output += Convert.ToChar(33 + (int)c + key - 126);
+                    }
+                    else
+                    {
+                        output += Convert.ToChar((int)c + key);
+                    }
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+        public string DecryptASCII(string input, int key)
+        {
+            string output = "";
+
+            foreach (char c in input)
+            {
+                if (IsASCIIChar(c))
+                {
+                    if ((int)c - key < 32)
+                    {
+                        output += Convert.ToChar((int)c - key + 93);
+                    }
+                    else
+                    {
+                        output += Convert.ToChar((int)c - key);
+                    }
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+    }
+}
--- CaesarCipher/Form1.cs
+++ CaesarCipher/Form1.cs
@@ -1,174 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CaesarCipher
 {
     public partial class Form1 : Form
     {
-        char[] Letters =
-        {
-            'A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k',
-            'L','l','M','m','N','n','O','o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v',
-            'W','w','X','x','Y','y','Z','z'
-        }; //Array of big and small letters
-
-        
+        //the quick brown fox jumps over the lazy dog
+        //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
         public Form1()
         {
             InitializeComponent();
         }
 
-        // Array
-
-        protected string EncryptArray(string input, int key)
-        {
-            string output = "";
-            int letterIndex, EncryptedLetterIndex;
-            foreach (char c in input)
-            {
-                letterIndex = Array.IndexOf(Letters, c); //Get index of required letter in Letters array
-                if (letterIndex == -1)
-                {
-                    output += c;
-                    continue;
-                }
-                else if (letterIndex <= Letters.Length - key * 2)
-                {
-                    output += Letters[letterIndex + key * 2].ToString();
-                    continue;
-                }
-                else if (letterIndex >= Letters.Length - key * 2) //
-                {
-                    EncryptedLetterIndex = letterIndex - (Letters.Length - key * 2);
-                    output += Letters[EncryptedLetterIndex].ToString();
-                }
-            }
-
-            return output;
-        }
-
-        protected string DecryptArray(string input, int key)
-        {
-            string output = "";
-
-            int letterIndex, EncryptedLetterIndex;
-            foreach (char c in input)
-            {
-                letterIndex = Array.IndexOf(Letters, c); //Get index of required letter in Letters array
-                if (letterIndex == -1) //If not letter
-                {
-                    output += c;
-                    continue;
-                }
-                else if (letterIndex - (key * 2) < 0) //If letter is in range
-                {
-                    output += Letters[Letters.Length + (letterIndex-key*2)].ToString();
-                    continue;
-                }
-                else if (letterIndex - key * 2 > 0) //If letter is out of range
-                {
-                    EncryptedLetterIndex = letterIndex -  key * 2;
-                    output += Letters[EncryptedLetterIndex].ToString();
-                    continue;
-                }
-                else 
-                {
-                    output += Letters[0];
-                }
-            }
-
-            return output;
-
-        }
-
-        //ASCII
-
-        protected static bool IsASCIILetter(char c)
-        {
-            return ((int)c >= 65 && (int)c <= 90 || (int)c >= 97 && (int)c <= 122); 
-            //a - 65, z = 90, A = 97, Z = 122;
-        }
-
-        protected static string EncryptASCII_Letters(string input, int key)
-        {
-            string output = "";
-
-            foreach(char c in input)
-            {
-                if (IsASCIILetter(c)) 
-                {
-                    if((int)c+key > 90 && (int)c+ key < 97) //If big letter out of range
-                    {
-                        output += Convert.ToChar(64 + ((int)c + key - 90));
-                        continue;
-                    }
-                    else if ((int)c + key > 122) //If small letter out of range
-                    {
-                        output += Convert.ToChar(96 + ((int)c + key) - 122);
-                        continue;
-                    }
-                    else //In range
-                    {
-                        output += Convert.ToChar(((int)c + key));
-                    }
-                }
-                else //not a letter
-                {
-                    output += c;
-                }
-            }
-
-
-            return output;
-        }
-
-        protected string DecryptASCII_Letters(string input, int key)
-        {
-            string output = "";
-
-            foreach(char c in input)
-            {
-                if(IsASCIILetter(c))
-                {
-                    if ((int)c - key < 65) //If big letter out of range
-                    {
-                        output += Convert.ToChar((int)c-key+26);
-                        continue;
-                    }
-                    else if ((int)c - key < 97 && (int)c - key > 90) //If small letter out of range
-                    {
-                        output += Convert.ToChar((int)c - key + 26);
-                        continue;
-                    }
-                    else
-                    {
-                        output += Convert.ToChar((int)c - 3);
-                    }
-                    
-
-                }
-                else
-                {
-                    output += c;
-                }
-            }
-
-            return output;
-        }
-
+        Caesar cc = new Caesar();
 
         //EVENTS 
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.textBox2.Text = EncryptASCII_Letters(this.textBox1.Text, (int)this.numericUpDown1.Value);
+            this.textBox2.Text = cc.EncryptASCII(this.textBox1.Text, (int)this.numericUpDown1.Value);
             //this.textBox2.Text += EncryptArray(this.textBox1.Text, (int)this.numericUpDown1.Value);
 
         }
@@ -177,7 +27,7 @@ namespace CaesarCipher
         {
             //this.textBox1.Text = DecryptArray(this.textBox2.Text, (int)this.numericUpDown1.Value);
             //EncryptArray(this.textBox2.Text, 26-(int)this.numericUpDown1.Value - works too
-            this.textBox1.Text = DecryptASCII_Letters(this.textBox2.Text, (int)this.numericUpDown1.Value);
+            this.textBox1.Text = cc.DecryptASCII(this.textBox2.Text, (int)this.numericUpDown1.Value);
         }
     }
 }
